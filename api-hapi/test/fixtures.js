0218cd768dcdbd64bcd9d61bd9e2c636e6ad5ac5
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-05-31 14:22:59 +0200

    added new fixtures, added poi tests all pass

--- api-hapi/test/fixtures.js
+++ api-hapi/test/fixtures.js
@@ -1,8 +1,8 @@
 export const maggie = {
-    firstName: "Maggie",
-    lastName: "Simpson",
-    email: "maggie@simpson.com",
-    password: "secret",
+  firstName: "Maggie",
+  lastName: "Simpson",
+  email: "maggie@simpson.com",
+  password: "secret"
 };
 
 export const testUsers = [
@@ -10,20 +10,62 @@ export const testUsers = [
     firstName: "Homer",
     lastName: "Simpson",
     email: "homer@simpson.com",
-    password: "secret",
+    password: "secret"
   },
   {
     firstName: "Marge",
     lastName: "Simpson",
     email: "marge@simpson.com",
-    password: "secret",
+    password: "secret"
   },
   {
     firstName: "Bart",
     lastName: "Simpson",
     email: "bart@simpson.com",
-    password: "secret",
-  },
+    password: "secret"
+  }
 ];
-  
-  
+
+export const regensburg = {
+  name: "Regensburg"
+};
+
+export const rgbdetails = {
+  "description": "Die bayerische Stadt Regensburg liegt an der Donau im Südosten Deutschlands und ist für ihr gut erhaltenes mittelalterliches Zentrum bekannt. Eines ihrer Wahrzeichen ist die Steinerne Brücke aus dem 12. Jh. Sie ist 310 m lang, hat 16 Bögen und verbindet den Stadtteil Stadtamhof mit der Altstadt. Ein weiteres wichtiges Wahrzeichen ist der gotische Dom aus dem 13. Jh. mit seinen Zwillingstürmen. Hier sind die Regensburger Domspatzen zu Hause. In der Walhalla, einer im Stil des Parthenon erbauten Gedenkstätte östlich der Stadt, werden bedeutende deutsche Persönlichkeiten geehrt.",
+  "latitude": 49.0134297,
+  "longitude": 12.1016236
+};
+
+export const weiden = {
+  name: "Weiden i.d.Opf."
+};
+
+export const testPois = [
+  {
+    name: "Regensburg"
+  },
+  {
+    name: "Weiden i.d.Opf."
+  },
+  {
+    name: "Amberg"
+  }
+];
+
+export const testDetails = [
+  {
+    "description": "Die bayerische Stadt Regensburg liegt an der Donau im Südosten Deutschlands und ist für ihr gut erhaltenes mittelalterliches Zentrum bekannt. Eines ihrer Wahrzeichen ist die Steinerne Brücke aus dem 12. Jh. Sie ist 310 m lang, hat 16 Bögen und verbindet den Stadtteil Stadtamhof mit der Altstadt. Ein weiteres wichtiges Wahrzeichen ist der gotische Dom aus dem 13. Jh. mit seinen Zwillingstürmen. Hier sind die Regensburger Domspatzen zu Hause. In der Walhalla, einer im Stil des Parthenon erbauten Gedenkstätte östlich der Stadt, werden bedeutende deutsche Persönlichkeiten geehrt.",
+    "latitude": 49.0134297,
+    "longitude": 12.1016236
+  },
+  {
+    "description": "Weiden in der Oberpfalz ist eine kreisfreie Stadt im ostbayerischen Regierungsbezirk Oberpfalz. Sie liegt 100 km östlich von Nürnberg und 35 km westlich der Grenze zu Tschechien am Fluss Waldnaab.",
+    "latitude": 49.674364,
+    "longitude": 12.148934
+  },
+  {
+    "description": "Amberg ist eine kreisfreie Stadt im Regierungsbezirk Oberpfalz in Ostbayern und gehört zur Metropolregion Nürnberg. Sie ist Teil der „Bayerische Eisenstraße“, die zwischen Pegnitz und Regensburg historische Industrie- und Kulturdenkmäler verbindet.",
+    "latitude": 49.4403198,
+    "longitude": 11.8633445
+  }
+];
---
+++ api-hapi/test/poi-model-test.js
@@ -0,0 +1,58 @@
+import { EventEmitter } from "events";
+import { assert } from "chai";
+import { db } from "../src/models/db.js";
+import { testPois, regensburg } from "./fixtures.js";
+import { assertSubset } from "./test-utils.js";
+
+EventEmitter.setMaxListeners(25);
+
+suite("Point of Interest Model tests", () => {
+  setup(async () => {
+    db.init("mongo");
+    await db.poiStore.deleteAllPois();
+    for (let i = 0; i < testPois.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testPois[i] = await db.poiStore.addPoi(testPois[i]);
+    }
+  });
+
+  test("create a Point of Interest", async () => {
+    const poi = await db.poiStore.addPoi(regensburg);
+    assertSubset(regensburg, poi);
+    assert.isDefined(poi._id);
+  });
+
+  test("delete all Points of Interest", async () => {
+    let returnedPois = await db.poiStore.getAllPois();
+    assert.equal(returnedPois.length, 3);
+    await db.poiStore.deleteAllPois();
+    returnedPois = await db.poiStore.getAllPois();
+    assert.equal(returnedPois.length, 0);
+  });
+
+  test("get a Point of Interest - success", async () => {
+    const poi = await db.poiStore.addPoi(regensburg);
+    const returnedPoi = await db.poiStore.getPoiById(poi._id);
+    assertSubset(regensburg, poi);
+  });
+
+  test("delete One Point of Interest - success", async () => {
+    const id = testPois[0]._id;
+    await db.poiStore.deletePoiById(id);
+    const returnedPois = await db.poiStore.getAllPois();
+    assert.equal(returnedPois.length, testPois.length - 1);
+    const deletedPoi = await db.poiStore.getPoiById(id);
+    assert.isNull(deletedPoi);
+  });
+
+  test("get a Point of Interest - bad params", async () => {
+    assert.isNull(await db.poiStore.getPoiById(""));
+    assert.isNull(await db.poiStore.getPoiById());
+  });
+
+  test("delete One Point of Interest - fail", async () => {
+    await db.poiStore.deletePoiById("bad-id");
+    const allPois = await db.poiStore.getAllPois();
+    assert.equal(testPois.length, allPois.length);
+  });
+});
