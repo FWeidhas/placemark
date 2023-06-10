6328e8ae4df3603c751fadd0834a8b851db71b70
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-10 13:04:01 +0200

    added api tests, folder models test, servicejs

--- api-hapi/test/models/details-model-test.js
+++ api-hapi/test/models/details-model-test.js
@@ -1,7 +1,7 @@
 import { assert } from "chai";
-import { db } from "../src/models/db.js";
-import { testPois, testDetails, weiden, regensburg, rgbdetails, testUsers } from "./fixtures.js";
-import { assertSubset } from "./test-utils.js";
+import { db } from "../../src/models/db.js";
+import { testPois, testDetails, weiden, regensburg, rgbdetails, testUsers } from "../fixtures.js";
+import { assertSubset } from "../test-utils.js";
 
 suite("Details Model tests", () => {
 
