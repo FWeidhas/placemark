7211956759a23d63ca63bc617f672121abbcd133
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-10 13:55:00 +0200

    added placemark user apis, tests pass

--- api-hapi/test/api/placemark-service.js
+++ api-hapi/test/api/placemark-service.js
@@ -8,5 +8,25 @@ export const placemarkService = {
   async createUser(user) {
     const res = await axios.post(`${this.placemarkUrl}/api/users`, user);
     return res.data;
+  },
+
+  async getUser(id) {
+    const res = await axios.get(`${this.placemarkUrl}/api/users/${id}`);
+    return res.data;
+  },
+
+  async getAllUsers() {
+    const res = await axios.get(`${this.placemarkUrl}/api/users`);
+    return res.data;
+  },
+
+  async deleteAllUsers() {
+    const res = await axios.delete(`${this.placemarkUrl}/api/users`);
+    return res.data;
+  },
+
+  async deleteOne(id) {
+    const res = await axios.delete(`${this.placemarkUrl}/api/users/${id}`);
+    return res.data;
   }
 }
