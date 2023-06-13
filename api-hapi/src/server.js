22a877d42d1f36226ffdf2811886875992fcb279
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-13 17:30:54 +0200

    added jwt utils, updated server js

--- api-hapi/src/server.js
+++ api-hapi/src/server.js
@@ -8,11 +8,13 @@ import dotenv from "dotenv";
 import Joi from "joi";
 import Inert from "@hapi/inert";
 import HapiSwagger from "hapi-swagger";
+import jwt from "hapi-auth-jwt2";
 
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
 import { apiRoutes } from "./api-routes.js";
+import { validate } from "./api/jwt-utils.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -36,11 +38,15 @@ async function init() {
     host: "localhost",
   });
 
+  await server.register(Inert);
+  await server.register(Vision);
+  await server.register(Cookie);
+  await server.register(jwt);
+
   server.validator(Joi);
   await server.register([
     Inert,
     Vision,
-    Cookie,
     {
       plugin: HapiSwagger,
       options: swaggerOptions,
@@ -58,7 +64,6 @@ async function init() {
     layout: true,
     isCached: false,
   });
-  db.init("mongo");
 
   server.auth.strategy("session", "cookie", {
     cookie: {
@@ -69,8 +74,15 @@ async function init() {
     redirectTo: "/",
     validate: accountsController.validate,
   });
+  server.auth.strategy("jwt", "jwt", {
+    key: process.env.cookie_password,
+    validate: validate,
+    verifyOptions: { algorithms: ["HS256"] }
+  });
   server.auth.default("session");
   
+  db.init("mongo");
+
   server.route(apiRoutes);
   server.route(webRoutes);
 
