7766e014241be31256d2c955b54836a005dc31b4
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-23 14:16:14 +0200

    added image, first components

---
+++ frontend-svelte/src/lib/TitleBar.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title = '';
+    export let subTitle = '';
+</script>
+
+<div class="box has-text-centered columns m-2">
+    <div class="column">
+        <div class="title is-5">{title}</div>
+        <div class="subtitle is-5">{subTitle}</div>
+    </div>
+    <div class="column">
+        <i style="font-size: 48px; color: rgb(74, 180, 118);"  class="far fa-map"></i>
+        <i style="font-size: 48px; color: rgb(153, 14, 14);" class="fas fa-map-marker-alt"></i>
+        <div class="is-size-7">Placemark 0.1</div>
+    </div>
+</div>
--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<div class="notification is-info">
-    Hello from Login!
+<script>
+  import Header from '../lib/Header.svelte';
+</script>
+
+<Header />
+
+<div class="columns is-vcentered content">
+  <div class="column has-text-centered">
+    <img width="80%" src="/fishing-boat.jpg" alt="fishing" />
+</div>
+  <div class="column">
+      <h1 class="title">Organise your favourite fishing spots</h1>
+      <p>Post your spots with description, location and pictures</p>
   </div>
+</div>
