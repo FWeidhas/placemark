6dc66a7b9e4a1e689b62790b28c62dc30989a15d
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-23 14:58:54 +0200

    added axios and placemark-service.js

---
+++ frontend-svelte/src/services/placemark-service.js
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import axios from "axios";
+
+export const placemark = {
+    baseUrl: "http://localhost:3000",
+
+    // @ts-ignore
+    async login(email, password) {
+        try {
+            const response = await axios.post(`${this.baseUrl}/api/users/authenticate`, { email, password });
+            axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.token;
+            if (response.data.success) {
+                return true;
+            }
+            return false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    },
+
+    async logout() {
+        axios.defaults.headers.common["Authorization"] = "";
+        localStorage.removeItem("donation");
+    },
+
+    // @ts-ignore
+    async signup(firstName, lastName, email, password) {
+        try {
+            const userDetails = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            };
+            await axios.post(this.baseUrl + "/api/users", userDetails);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+};
