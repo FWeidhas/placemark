e881b0e2ff965a660a916888071038d146f6d131
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-23 15:12:50 +0200

    logout works

--- frontend-svelte/src/lib/MainNavigator.svelte
+++ frontend-svelte/src/lib/MainNavigator.svelte
@@ -1,11 +1,11 @@
 <div class="box has-text-centered columns m-2">
-    <a href="/donate" class="column is-4">
+    <a href="/addpoi" class="column is-4">
         <i class="fas fa-plus fa-3x" style="color:rgb(153, 196, 74)" />
     </a>
-    <a href="/report" class="column is-4">
+    <a href="/dashboard" class="column is-4">
         <i class="fas fa-th-list fa-3x" style="color:rgb(63, 122, 139)" />
     </a>
-    <a href="/" class="column is-4">
+    <a href="/logout" class="column is-4">
         <i class="fas fa-sign-out-alt fa-3x" style="color:rgb(156, 70, 128)" />
     </a>
 </div>
