7e0fa9ee92c5ee63c18aaae61690ac8dc39ea7de
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-23 15:17:44 +0200

    added store and added user to titlebar

--- frontend-svelte/src/lib/TitleBar.svelte
+++ frontend-svelte/src/lib/TitleBar.svelte
@@ -1,6 +1,7 @@
 <script>
-    export let title = '';
-    export let subTitle = '';
+    import { user } from "../stores.js";
+    export let title = "";
+    export let subTitle = "";
 </script>
 
 <div class="box has-text-centered columns m-2">
@@ -9,8 +10,12 @@
         <div class="subtitle is-5">{subTitle}</div>
     </div>
     <div class="column">
-        <i style="font-size: 48px; color: rgb(74, 180, 118);"  class="far fa-map"></i>
-        <i style="font-size: 48px; color: rgb(153, 14, 14);" class="fas fa-map-marker-alt"></i>
-        <div class="is-size-7">Placemark 0.1</div>
+        <i style="font-size: 48px; color: rgb(74, 180, 118);"  class="far fa-map" />
+        <i style="font-size: 48px; color: rgb(153, 14, 14);" class="fas fa-map-marker-alt" />
+        {#if $user.email}
+            <div class="is-size-7">{$user.email}</div>
+        {:else}
+            <div class="is-size-7">Placemark 5.0</div>
+        {/if}
     </div>
 </div>
