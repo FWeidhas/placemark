e8536fb453fecdd2bb96c74bbc2aa5f330768b2d
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-27 17:31:27 +0200

    formatted code

--- frontend-svelte/src/lib/AddPoiForm.svelte
+++ frontend-svelte/src/lib/AddPoiForm.svelte
@@ -3,14 +3,14 @@
     import { placemarkService } from "../services/placemark-service";
 	import { user } from "../stores";
   
-      let name ="";
-  
-      let categories = ["River", "Pond", "Sea", "Lake"];
-      let selectedCategory = "Select category";
+    let name ="";
 
-      let message = "Add your spot with name and category";
-  
-      async function addpoi() {
+    let categories = ["River", "Pond", "Sea", "Lake"];
+    let selectedCategory = "Select category";
+
+    let message = "Add your spot with name and category";
+
+    async function addpoi() {
         if (!selectedCategory || !name || selectedCategory === "Select category") {
             message = "Please select name and category";
             return;
@@ -28,32 +28,31 @@
         } else {
             message = "Adding not completed - some error occurred";
         }
-        }
-
-  </script>
+    }
+</script>
   
-  <form on:submit|preventDefault={addpoi}>
-      <div class="field">
-          <label class="label" for="name">Point of Interest Name</label>
-          <input bind:value={name} class="input" id="name" name="name" type="text" />
-      </div>
-      <div class="field">
-          <div class="select">
-              <select bind:value={selectedCategory}>
-                  <option disabled hidden>Select category</option>
-                  {#each categories as category}
-                      <option>{category}</option>
-                  {/each}
-              </select>
-          </div>
-      </div>
-      <div class="field">
-          <div class="control">
-              <button class="button is-link is-light">Add</button>
-          </div>
-      </div>
-      <div class="box">
-          {message}
-      </div>
-  </form>
+<form on:submit|preventDefault={addpoi}>
+    <div class="field">
+        <label class="label" for="name">Point of Interest Name</label>
+        <input bind:value={name} class="input" id="name" name="name" type="text" />
+    </div>
+    <div class="field">
+        <div class="select">
+            <select bind:value={selectedCategory}>
+                <option disabled hidden>Select category</option>
+                {#each categories as category}
+                    <option>{category}</option>
+                {/each}
+            </select>
+        </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <button class="button is-link is-light">Add</button>
+        </div>
+    </div>
+    <div class="box">
+        {message}
+    </div>
+</form>
   
