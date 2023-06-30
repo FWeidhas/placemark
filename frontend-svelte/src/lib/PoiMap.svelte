646923af5300a890530d82d0b53e6a30d381bf32
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-30 11:27:13 +0200

    updated header, navigator and map works

---
+++ frontend-svelte/src/lib/PoiMap.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "leaflet/dist/leaflet.css";
+    import { LeafletMap } from "../services/leaflet-map.js";
+    import { onMount } from "svelte";
+
+    const mapConfig = {
+        location: { lat: 49.0134297, lng: 12.1016236 },
+        zoom: 8,
+        minZoom: 1
+    };
+
+    onMount(async () => {
+        const map = new LeafletMap("poi-map", mapConfig);
+    });
+</script>
+
+<div class="box" id="poi-map" style="height:75vh" />
---
+++ frontend-svelte/src/routes/map/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { beforeUpdate } from "svelte";
+    import Header from "../../lib/Header.svelte";
+    import MainNavigator from "../../lib/MainNavigator.svelte";
+    import PoiMap from "../../lib/PoiMap.svelte";
+    import { placemarkService } from "../../services/placemark-service.js";
+
+    beforeUpdate(() => {
+        placemarkService.reload();
+    });
+</script>
+
+<Header>
+    <MainNavigator />
+</Header>
+
+<PoiMap />
