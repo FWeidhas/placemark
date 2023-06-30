43e0211cfda8a485032dcfe2cde98f8ec6fc5e4d
Felix Weidhas <felix.weidhas@st.oth-regensburg.de>
2023-06-30 16:32:47 +0200

    removed comment

--- frontend-svelte/src/services/leaflet-map.js
+++ frontend-svelte/src/services/leaflet-map.js
@@ -84,7 +84,6 @@ export class LeafletMap {
     }
 
     clearMarker() {
-        // remove all markers from each layer
         for (let layer in this.overlays) {
           this.overlays[layer].clearLayers();
         }
